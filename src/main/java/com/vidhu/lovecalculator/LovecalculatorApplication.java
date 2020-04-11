adc5d2a3cc8329113415f46caae5a9e0345e9d54
Arun Viswanathan <arunplr@gmail.com>
2020-04-11 13:23:26 -0400

    first commit

---
+++ src/main/java/com/vidhu/lovecalculator/LovecalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.vidhu.lovecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovecalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovecalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vidhu/lovecalculator/LovecalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidhu.lovecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovecalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
